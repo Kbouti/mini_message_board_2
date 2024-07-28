748aaac6cac692422aa6710c4b2081726e737e59
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-07-28 11:41:46 -0700

    Create navbar

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,9 @@ const {Router} = require("express");
 
 const indexRouter = Router();
 
+
+
+
 indexRouter.get("/", (req, res) => {
     // res.send("You're in the index section");
     res.render("home", {message: "Baddest node website in the webs"});
