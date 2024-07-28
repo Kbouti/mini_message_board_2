8d4595ec5e65d0b796cff4e0c4d271643866171a
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-07-28 13:24:43 -0700

    Begin styling app

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,11 +3,24 @@ const {Router} = require("express");
 const indexRouter = Router();
 
 
-
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amanda",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+]
 
 indexRouter.get("/", (req, res) => {
     // res.send("You're in the index section");
-    res.render("home", {subtitle: "Baddest node website in the webs"});
+    res.render("home", {subtitle: "Baddest node website in the webs",
+        messages: messages
+    });
 })
 
 module.exports = indexRouter;
