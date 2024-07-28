a80df39ffb6b0ead66a9487ef1a49c44855895ea
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-07-28 14:11:30 -0700

    Create new message form

--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -3,7 +3,7 @@ const {Router} = require("express");
 const newMessageRouter = Router();
 
 newMessageRouter.get("/", (req, res) => {
-    res.render("newMessage", {subtitle: "This is where you would create a new message"});
+    res.render("newMessage", {subtitle: "Create message:"});
 })
 
 module.exports = newMessageRouter;
