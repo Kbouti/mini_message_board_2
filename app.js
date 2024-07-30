a77205250e5c86833d0f0b9ca277b79e271ecf12
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-07-30 10:17:50 -0700

    Remove newMessageRouter file and consolidate into indexRouter

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 // Configure view engine
-const path = require("path")
+const path = require("path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -16,18 +16,14 @@ require("dotenv").config();
 // Configure yrlencoded middleware to allow access to form submission req.body
 app.use(express.urlencoded({ extended: true }));
 
-
-
 const indexRouter = require("./routes/indexRouter");
-const newMessageRouter = require("./routes/newMessageRouter");
-
 // app.get("/", (req, res) => res.send("Hello, world!"));
-
-
 app.use("/", indexRouter);
-app.use("/new", newMessageRouter);
-// We're gonna try to move this^^ to indexrouter
 
 const PORT = process.env.PORT || 3000;
 const MODE = process.env.MODE || "development";
-app.listen(PORT, () => console.log(`My first Express app - mode is ${MODE} listening on port ${PORT}!`));
+app.listen(PORT, () =>
+  console.log(
+    `My first Express app - mode is ${MODE} listening on port ${PORT}!`
+  )
+);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,33 +1,38 @@
-const {Router} = require("express");
+const { Router } = require("express");
 
 const indexRouter = Router();
 
-
-const testVariable = "test-test"
-
 const messages = [
-    {
-        text: "Hi there!",
-        user: "Amanda",
-        added: new Date()
-    },
-    {
-        text: "Hello World!",
-        user: "Charles",
-        added: new Date()
-    }
-]
+  {
+    text: "Hi there!",
+    user: "Amanda",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
 indexRouter.get("/", (req, res) => {
-    // res.send("You're in the index section");
-    res.render("home", {subtitle: "Messages:",
-        messages: messages
-    });
-})
+  // res.send("You're in the index section");
+  res.render("home", { subtitle: "Messages:", messages: messages });
+});
 
-// **************************************************************************************************
-//  export {indexRouter, messages, testVariable};
-// Tried the above^^ and it broke the app. Below export syntax didn't work either. 
-// The goal was to export messages to the newMessageRouter file
-module.exports = indexRouter, messages, testVariable;
-// **************************************************************************************************
+indexRouter.get("/new", (req, res) => {
+  console.log(`New message route activated`);
+  res.render("newMessage", { subtitle: "Create message:" });
+});
+
+indexRouter.post("/new", (req, res) => {
+  console.log(`req.body.text: ${req.body.text}`);
+
+  // ************************************************************************************
+  // Next up we need to push our new message object to the messages array
+  // ************************************************************************************
+
+  res.send("You submitted the form! ");
+});
+
+module.exports = indexRouter;
