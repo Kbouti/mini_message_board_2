4c764221f3606c19f4b7093a35f9136d17163f50
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-04 11:26:03 -0700

    Attempt to format date

--- database/queries.js
+++ database/queries.js
@@ -8,17 +8,55 @@ async function getAllMessages() {
 
 async function createRow(newEntry) {
 console.log(`creating a new row. `)
-    const sql = `insert into messages (text, author, date, time) values (${newEntry.text}, ${newEntry.author}, ${newEntry.date}, ${newEntry.time});`
+
+
+function formatDate(value) {
+    let date = new Date(value);
+    const day = date.toLocaleString('default', { day: '2-digit' });
+    const month = date.toLocaleString('default', { month: 'short' });
+    const year = date.toLocaleString('default', { year: 'numeric' });
+    return day + '-' + month + '-' + year;
+}
+
+
+function convertDate(value) {
+    function pad(s) { return (s < 10) ? '0' + s : s; }
+    var d = new Date(value)
+    return [pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear()].join('/')
+  }
+
+
+let rawDate = newEntry.date;
+let formattedDate = formatDate(rawDate);
+let convertedDate = convertDate(rawDate);
+console.log(`rawDate: ${rawDate}`);
+console.log(`formattedDate: ${formattedDate}`);
+console.log(`convertedDate: ${convertedDate}`);
+
+
+const newDate = new Date(rawDate);
+console.log(`newDate: ${newDate}`);
+
+
+let rawTime = newEntry.time;
+
+
+
+
+    const sql = `insert into messages (text, author, date) values (${newEntry.text}, ${newEntry.author}, ${convertedDate});`
 console.log(`sql: ${sql}`);
 
 // ******************************************************************************************************
 // OK! The current issue is that it just isn't liking our date format. That seems fixable. 
+
+// SQL isn't liking the date we're trying to give it. I think it just wants DD/MM/YYYY
+// Okkkk now it it's saying column "message text here" doesn't exist???
 // ******************************************************************************************************
 
 
-    // const response = await pool.query(sql);
-    // console.log(response);
-    // return;
+    const response = await pool.query(sql);
+    console.log(response);
+    return true;
 }
 
 
